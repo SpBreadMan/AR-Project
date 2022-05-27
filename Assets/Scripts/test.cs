9d68e53a33b2ca7bdec6634de26bf027281205da
SpBreadMan <95220663+SpBreadMan@users.noreply.github.com>
2022-05-27 19:05:34 +0800

    001

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private string aa = "asdasdasd";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
